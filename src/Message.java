7f0b47df5b90748f5e3678852ea4afa7c0018ae3
Ntinos <konstantinides.kon@gmail.com>
2022-05-12 16:56:20 +0300

    Update Message.java

--- src/Message.java
+++ src/Message.java
@@ -28,7 +28,7 @@ public class Message extends Define{
 		System.out.println("\tmain <method> <input-file> <output-file>\n\n");
 		System.out.println("where: ");
 		System.out.println("\t<method> = breadth|depth|best|astar");
-		System.out.println("\t<input-file> is a file containing the puzzle description with the highest card being card no." + N);
+		System.out.println("\t<input-file> is a file containing the puzzle description");
 		System.out.println("\t<output-file> is the name of the file where the solution will be written.\n");
 		System.exit(1);
 	}
